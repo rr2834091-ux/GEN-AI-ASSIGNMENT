21d4c97310fc4fdd5c534c081248304626b43211
RAGUL R <rr2834091@gmail.com>
2026-06-20 23:34:52 -0700

    Pandas OUTPUT.

---
+++ PANDAS/X-OUPUT.py
@@ -0,0 +1,50 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+====================== RESTART: C:/Python314/Series das.py =====================
+Vegitables:
+--------------------
+0           Onion
+1          Tomato
+2           Peans
+3          Carrot
+4          Potato
+5          Cabage
+6    Ladiesfinger
+7        beetroot
+8          Radish
+9         Pumpkin
+dtype: object
+<class 'list'>
+Cabage
+Tomato
+5          Cabage
+6    Ladiesfinger
+7        beetroot
+8          Radish
+9         Pumpkin
+dtype: object
+1      Tomato
+3      Carrot
+5      Cabage
+7    beetroot
+9     Pumpkin
+dtype: object
+Vegitables weights
+----------------------------------
+['Onion', 'Tomato', 'Peans', 'Carrot', 'Potato', 'Cabage', 'Ladiesfinger', 'beetroot', 'Radish', 'Pumpkin']
+beetroot
+Radish
+Potato
+Vegitable: prices:
+Onion           160
+Tomato          130
+Peans            90
+Carrot           40
+Potato          120
+Cabage           60
+Ladiesfinger     50
+beetroot        150
+Radish          200
+Pumpkin         100
+dtype: object
