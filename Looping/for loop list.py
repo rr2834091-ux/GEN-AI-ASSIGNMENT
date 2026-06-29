86ef9345fbfa4e3d8b452ad806bf78388d81f5d3
RAGUL R <rr2834091@gmail.com>
2026-06-29 19:39:17 +0530

    Prifix,append,split

---
+++ Looping/for loop list.py
@@ -0,0 +1,17 @@
+#print only second name in list:
+STUD=['alvinas sadhana','hepzibah vinithra','stacey mithal','sowmiya angel','nimmy rachel']
+for i in STUD:
+    print(i.split(' ')[1])
+print('-'*10)   
+#print name in Upper list:
+STUD=['alvinas sadhana','hepzibah vinithra','stacey mithal','sowmiya angel','nimmy rachel']
+for i in STUD:
+    print(i.split(' ')[1].upper())
+print('-'*10)
+#Stored elements in a list:
+STUD=['alvinas sadhana','hepzibah vinithra','stacey mithal','sowmiya angel','nimmy rachel']
+for i in STUD:
+    print(i.split(' ')[1].upper())
+    STUD=[]
+    STUD.append
+    
