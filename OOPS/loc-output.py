1fbc00445ba57355bb1ae9a835f5ed73a7504bd2
RAGUL R <rr2834091@gmail.com>
2026-07-01 16:03:35 +0530

    student details

---
+++ OOPS/loc-output.py
@@ -0,0 +1,10 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+==================== RESTART: C:/Python314/local variable.py ===================
+Student Details:
+Name : Ragul
+Age : 22
+Course : Gen AI
+City : Chennai
+phnum : 919293949596
---
+++ OOPS/local variable.py
@@ -0,0 +1,24 @@
+print('Student Details:')
+class Student:
+    def Name(self):
+        name='Ragul'
+        print('Name :',name)
+    def Age(self):
+        Age=22
+        print('Age :',Age) 
+    def Course(self):
+        Course='Gen AI'
+        print('Course :',Course)
+    def City(self):
+        City='Chennai'
+        print('City :',City)
+    def phnum(self):
+        phnum=+919293949596
+        print('phnum :',phnum)
+
+obj=Student()
+obj.Name()
+obj.Age()
+obj.Course()
+obj.City()
+obj.phnum()
