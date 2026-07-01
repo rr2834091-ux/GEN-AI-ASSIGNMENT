06f3c7dcb2d3154e4a2eb8a53b88f79f38bde6da
RAGUL R <rr2834091@gmail.com>
2026-07-01 21:30:58 +0530

    Animals Sounds in poly

---
+++ OOPS/poly 1.py
@@ -0,0 +1,20 @@
+class Animal:
+    def sound(self):
+        print('animals make different sounds:')
+class Dog(Animal):
+    def sound(self):
+        print('Dog barks')
+class Cat(Animal):
+    def sound(Self):
+        print('Cat Meows')
+class Cow(Animal):
+    def sound(self):
+        print('Cow moos')
+
+dog=Dog()
+cat=Cat()
+cow=Cow()
+
+dog.sound()
+cat.sound()
+cow.sound()
