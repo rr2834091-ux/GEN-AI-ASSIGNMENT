93e4de497766277dd456e68b22425abc6b3081ee
RAGUL R <rr2834091@gmail.com>
2026-07-04 21:07:01 +0530

    Add files via upload

---
+++ NUMPY/mat-output.py
@@ -0,0 +1,20 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+===================== RESTART: C:/Python314/matrix-numpy.py ====================
+Addition:
+[[ 5  7  9]
+ [10 12 14]]
+Subtraction:
+[[-3 -3 -3]
+ [-2 -2 -2]]
+Multiplication:
+[[ 4 10 18]
+ [24 35 48]]
+Divison:
+[[0.25       0.4        0.5       ]
+ [0.66666667 0.71428571 0.75      ]]
+Transpose:
+[[1 4]
+ [2 5]
+ [3 6]]
---
+++ NUMPY/matrix-numpy.py
@@ -0,0 +1,14 @@
+#numpy using matrix program:
+import numpy as np
+x1=np.array([[1,2,3],[4,5,6]])
+x2=np.array([[4,5,6],[6,7,8]])
+print('Addition:')
+print(x1+x2)
+print('Subtraction:')
+print(x1-x2)
+print('Multiplication:')
+print(x1*x2)
+print('Divison:')
+print(x1/x2)
+print('Transpose:')
+print(x1.T)
